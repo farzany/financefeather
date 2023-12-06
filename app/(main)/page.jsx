d8790efc53ddc1290de472c96cbd36182c5e9702
Talha2000 <amjad_maqsood90@hotmail.com>
2023-12-06 02:38:40 -0500

    created leftNav

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -1,6 +1,8 @@
 import AutoTransactions from '@/components/AutoTransactions';
 import Distributions from '@/components/Distributions';
 import Goals from '@/components/Goals';
+import LeftNav from '@/components/LeftNav';
+import GeneralInfo from '@/components/GeneralInfo';
 import React from 'react';
 
 // Uncomment your component
@@ -10,6 +12,7 @@ export default async function Home() {
       {/* <Goals /> */}
       {/* <AutoTransactions /> */}
       {/* <Distributions /> */}
+      <LeftNav/>
     </main>
   );
 }
