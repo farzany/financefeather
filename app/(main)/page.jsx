7b780b09e8299d8d5051fcf549945952a92048a7
Farzan Yazdanjou <farzan.yazdanjou@gmail.com>
2023-12-05 19:31:11 -0500

    Boilerplate

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -1,9 +1,15 @@
+import AutoTransactions from '@/components/AutoTransactions';
+import Distributions from '@/components/Distributions';
+import Goals from '@/components/Goals';
 import React from 'react';
 
+// Uncomment your component
 export default async function Home() {
   return (
     <main className="relative flex justify-center items-center h-screen">
-      Hi
+      {/* <Goals /> */}
+      {/* <AutoTransactions /> */}
+      {/* <Distributions /> */}
     </main>
   );
 }
---
+++ components/AutoTransactions.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function AutoTransactions() {
+  return (
+    <div>
+      Auto Transactions
+    </div>
+  )
+}
